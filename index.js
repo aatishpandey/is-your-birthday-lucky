dad7dd362273ed2c881f9b0ffcf234ab9debc2b9
Aatish Pandey <32216307+aatishpandey@users.noreply.github.com>
2021-09-16 09:45:43 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkBtn = document.querySelector("#btn-check-number");
+const outputBox = document.querySelector("#output-box");
 
-function calculateSum(Bdate){
-    Bdate = Bdate.replaceAll("-","");
+
+function compareForLuck(summ, luckyNum) {
+    if (summ % luckyNum === 0) {
+        outputBox.innerText = "You birhday is lucky";
+    } else {
+        outputBox.innerText = "You're unlucky";
+    }
+};
+
+function checkLuckyNumber() {
+
+    const dOb = dateOfBirth.value;
+    const numberForCheck = luckyNumber.value;
+
+    if (dOb && numberForCheck) {
+        const sumOfDob = calculateSum(dOb);
+        compareForLuck(sumOfDob, numberForCheck);
+    } else {
+        outputBox.innerText = "Please enter both feilds";
+    }
+};
+
+function calculateSum(Bdate) {
+    Bdate = Bdate.replaceAll("-", "");
     let sum = 0;
-    for(let i=0;i<Bdate.length;i++)
-    {
-        sum += Number(Bdate.charAt(i));
+    for (let i of Bdate) {
+        sum += Number(i);
     }
     return sum;
-}
-
-function checkLuckyNumber{
-    const Bdate =  dateOfBirth.value;
-    const sum = calculateSum(Bdate);
-
-    
-}
+};
 
 
-checkBtn.addEventListener('click',function checkLuckyNumber(){
 
+checkBtn.addEventListener("click", checkLuckyNumber);
+// const dOb = dateOfBirth.value;
+// const numberForCheck = luckyNumber.value;
+// // console.log(dOb);
 
-})
-
+// if(dOb && numberForCheck){
+//     const sumOfDob = calculateSum(dOb);
+//     checkLuckyNumber(sumOfDob,numberForCheck);
+// }
+// else{
+//     outputBox.innerText = "Please enter both feilds";
+// }
